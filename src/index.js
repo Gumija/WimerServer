67c9ff074b24ed88f459e712c2c3d4e9d503dee8
Milán Jakab Gulyás <gumija6@gmail.com>
2017-04-29 11:32:18 +0200

    added google auth

--- src/index.js
+++ src/index.js
@@ -10,9 +10,54 @@ import GoogleAuth from 'passport-google-oauth2';
 import path from 'path';
 import DbIniter from './db/db';
 
-let app = express();
-let upload = multer({ dest: 'uploads/' });
+// API Access link for creating client ID and secret:
+// https://code.google.com/apis/console/
+var GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+  , GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+console.log('GOOGLE STUFF:', GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Google profile is
+//   serialized and deserialized.
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleAuth.Strategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  //NOTE :
+  //Carefull ! and avoid usage of Private IP, otherwise you will get the device_id device_name issue for Private IP during authentication
+  //The workaround is to set up thru the google cloud console a fully qualified domain name such as http://mydomain:3000/ 
+  //then edit your /etc/hosts local file to point on your private IP. 
+  //Also both sign-in button + callbackURL has to be share the same url, otherwise two cookies will be created and lead to lost your session
+  //if you use it.
+  callbackURL: "http://morning-stream-82096.herokuapp.com/auth/google/callback",
+  passReqToCallback: true
+},
+  function (request, accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
 
 let dbIniter = new DbIniter();
 dbIniter.initDB();
@@ -50,6 +95,8 @@ let highlights = {
      AND user_id = ?',
 }
 
+let app = express();
+let upload = multer({ dest: 'uploads/' });
 
 let buildFolderPath = process.env.DATABASE_URL ? path.resolve(__dirname, './', 'WimerReact/build')
   : path.resolve(__dirname, '..', 'WimerReact/build')
@@ -58,6 +105,32 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 // Serve static assets
 app.use(express.static(buildFolderPath));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google', passport.authenticate('google', {
+  scope: [
+    'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read']
+}));
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }));
+
+
 
 app.get('/highlight/:documentId/:userId', (req, res) => {
   dbIniter.query(mysql.format(highlights.selectByDocumentAndUser,
